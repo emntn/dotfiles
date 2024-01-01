43c3cbc221d16645b0de4cec18682e1f13026b3a
Emil Nuutinen <emil@emilnuutinen.com>
2024-01-01 15:08:22 +0200

    Add current year as a variable

--- .scripts/tmux/base.sh
+++ .scripts/tmux/base.sh
@@ -4,12 +4,13 @@ session="base"
 
 # current week as filename
 journal=journal_w$(date "+%V").md
+year=$(date "+%Y")
 
 tmux new-session -d -s $session
 
 window=1
 tmux rename-window -t $session:$window 'note'
-tmux send-keys -t $session:$window 'cd ~/notes && vim -O personal/journal/2023/'$journal' dev/rust/06_change_in_course.md' C-m
+tmux send-keys -t $session:$window 'cd ~/notes && vim -O personal/journal/'$year'/'$journal' dev/learning/01_new_chapter.md' C-m
 
 window=2
 tmux new-window -t $session:$window
