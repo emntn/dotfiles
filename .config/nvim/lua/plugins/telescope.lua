a96d11b517b5fbe6100d344e0b421f3b4225deba
Emil Nuutinen <26424780+emilnuutinen@users.noreply.github.com>
2024-10-11 17:03:06 +0300

    Change to ivy theme for builtins and cursor theme for ui-select.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,23 +4,15 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('telescope').load_extension('fzy_native')
-      require("telescope").load_extension("ui-select")
-
       local actions = require("telescope.actions")
       require("telescope").setup {
-        defaults = {
-          layout_config = {
-            horizontal = {
-              height = 0.9,
-              preview_cutoff = 160,
-              prompt_position = "bottom",
-              width = 0.9
-            }
-          },
+        defaults = require('telescope.themes').get_ivy {
+          layout_config = { height = 0.5 },
+          sorting_strategy = "ascending",
           mappings = {
             i = {
-              ["<C-l>"] = actions.select_vertical
+              ["<C-l>"] = actions.select_vertical,
+              ["<esc>"] = actions.close
             },
             n = {
               ["<C-l>"] = actions.select_vertical
@@ -30,18 +22,21 @@ return {
         extensions = {
           -- Use Telescope for Neovim core stuff like code actions etc...
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
+            require("telescope.themes").get_cursor {}
           }
         }
       }
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fr', builtin.spell_suggest, {})
+      vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fw', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fc', builtin.git_commits, {})
-      vim.keymap.set('n', '<leader>fb', builtin.git_bcommits, {})
+      vim.keymap.set('n', '<leader>fc', builtin.git_bcommits, {})
+      vim.keymap.set('n', '<leader>ft', builtin.builtin, {})
+
+      require('telescope').load_extension('fzy_native')
+      require("telescope").load_extension("ui-select")
     end
   },
   { "nvim-telescope/telescope-fzy-native.nvim" },
