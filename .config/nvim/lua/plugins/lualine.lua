be0dc9c67060e9aeb04da6393fccd7ea1a1ac156
Emil Nuutinen <26424780+emilnuutinen@users.noreply.github.com>
2024-10-12 01:31:38 +0300

    Add arkav/lualine-lsp-progress

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,12 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+      {
+        "linrongbin16/lsp-progress.nvim",
+        config = true,
+      },
+    },
     config = function()
       local function getWords()
         local validFileTypes = { md = true, txt = true, markdown = true }
@@ -34,7 +40,33 @@ return {
           lualine_a = { 'mode' },
           lualine_b = { 'branch' },
           lualine_c = { 'filename', 'diagnostics', 'diff' },
-          lualine_x = { 'encoding', 'filetype', { getWords } },
+          lualine_x = {
+            {
+              function()
+                return require("lsp-progress").progress({
+                  max_size = 80,
+                  format = function(messages)
+                    local active_clients =
+                        vim.lsp.get_active_clients()
+                    if #messages > 0 then
+                      return table.concat(messages, " ")
+                    end
+                    local client_names = {}
+                    for _, client in ipairs(active_clients) do
+                      if client and client.name ~= "" then
+                        table.insert(
+                          client_names,
+                          1,
+                          client.name
+                        )
+                      end
+                    end
+                    return table.concat(client_names, " ")
+                  end,
+                })
+              end,
+            },
+            'encoding', 'filetype', { getWords } },
           lualine_y = { { progress } },
           lualine_z = { 'location' }
         },
@@ -44,6 +76,14 @@ return {
         inactive_winbar = {},
         extensions = {}
       }
+
+      -- listen lsp-progress event and refresh lualine
+      vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+      vim.api.nvim_create_autocmd("User", {
+        group = "lualine_augroup",
+        pattern = "LspProgressStatusUpdated",
+        callback = require("lualine").refresh,
+      })
     end
   }
 }
