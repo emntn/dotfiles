a6cf7c0b6519151fa68c687e163def97f084c995
Emil <26424780+emntn@users.noreply.github.com>
2026-05-28 13:31:35 +0300

    Update base script for current situation

--- .scripts/tmux/base.sh
+++ .scripts/tmux/base.sh
@@ -6,14 +6,20 @@ session="base"
 journal=journal_w$(date "+%V").md
 year=$(date "+%Y")
 
-tmux new-session -d -s $session -d 'mutt'
-tmux split-window -h 'calopen'
+tmux new-session -d -s $session
+
+window=1
+tmux new-window -t $session:$window
+tmux rename-window -t $session:$window 'mutt'
+tmux send-keys -t $session:$window 'cd ~/Downloads && mutt' C-m
+tmux split-window -h
+tmux send-keys 'calopen' C-m
+tmux select-pane -L
 
 window=2
 tmux new-window -t $session:$window
 tmux rename-window -t $session:$window 'note'
-tmux send-keys -t $session:$window 'sh ~/.scripts/journal/load_journal.sh && cd ~/notes && vim -O personal/journal/'$year'/'$journal' personal/goals/new_life/01_planning_it.md' C-m
-
+tmux send-keys -t $session:$window 'sh ~/.scripts/journal/load_journal.sh && cd ~/notes && vim -O personal/journal/'$year'/'$journal' personal/goals/new_life/02_time_to_execute.md' C-m
 
 window=3
 tmux new-window -t $session:$window
