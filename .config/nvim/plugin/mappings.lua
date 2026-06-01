dfbb803279b36a682f7706adda7999a0a234ac98
Emil <26424780+emntn@users.noreply.github.com>
2026-06-01 13:52:19 +0300

    Disable manual inlay hint call at least for now

--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -84,18 +84,18 @@ vim.cmd([[
 -- Make "Y" yank till the end of line
 nmap('Y', 'y$')
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local bufnr = args.buf ---@type number
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client:supports_method('textDocument/inlayHint') then
-      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-      vim.keymap.set('n', '<C-i>', function()
-        vim.lsp.inlay_hint.enable(
-          not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
-          { bufnr = bufnr }
-        )
-      end, { buffer = bufnr })
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(args)
+--     local bufnr = args.buf ---@type number
+--     local client = vim.lsp.get_client_by_id(args.data.client_id)
+--     if client:supports_method('textDocument/inlayHint') then
+--       vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+--       vim.keymap.set('n', '<C-i>', function()
+--         vim.lsp.inlay_hint.enable(
+--           not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
+--           { bufnr = bufnr }
+--         )
+--       end, { buffer = bufnr })
+--     end
+--   end,
+-- })
